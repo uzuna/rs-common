628f435397f3d3410cbb22719a94018866868a65
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-11-05 08:22:17 +0900

    fix(example): preview ImageLoader

--- examples/wasm-preview/src/entrypoint.rs
+++ examples/wasm-preview/src/entrypoint.rs
@@ -13,5 +13,29 @@ pub fn init() -> Result<()> {
 pub fn start() -> std::result::Result<(), JsValue> {
     info!("Hello, wasm!");
     wasm_util::error!("This is a error.");
+
+    wasm_bindgen_futures::spawn_local(async {
+        match load_image().await {
+            Ok(elapsed) => info!("Image loaded in {} ms", elapsed),
+            Err(e) => wasm_util::error!("Failed to load image: {:?}", e),
+        }
+    });
     Ok(())
+}
+
+// 画像を読み込む非同期関数
+async fn load_image() -> Result<f64> {
+    let p = wasm_util::util::get_performance()?;
+    let start = p.now();
+
+    let loader = wasm_util::image::ImageLoader::new(
+        "https://rustacean.net/assets/rustacean-flat-happy.png",
+    )?;
+    let img = loader.await?;
+    info!("Image loaded: {}x{}", img.width(), img.height());
+    let el = wasm_util::util::get_element::<web_sys::HtmlElement>("image-target")?;
+    el.append_child(img.as_ref())
+        .map_err(|_| JsError::new("failed to append image"))?;
+    let elapsed = p.now() - start;
+    Ok(elapsed)
 }

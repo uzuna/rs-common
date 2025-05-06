67392cc7e5fde5a7488843c4e0df741d849a8505
Fumiya Fujinaka <uzuna.kf@gmail.com>
2025-05-06 18:02:06 +0900

    fix(iced): サブスレッド処理のベースを作成

--- examples/iced-learn/src/main.rs
+++ examples/iced-learn/src/main.rs
@@ -1,18 +1,23 @@
+use std::time::Instant;
+
 use iced::widget::{
     button, column, container, horizontal_space, row, slider, text, text_input, Column,
 };
-use iced::{border, Alignment, Element, Font, Task, Theme};
+use iced::{border, window, Alignment, Element, Font, Subscription, Task, Theme};
 use scene::systeminfo::{self, SystemInfoMsg};
 
 pub mod scene;
+pub mod server;
 
 pub fn main() -> iced::Result {
+    init();
     let s = iced::settings::Settings {
         default_font: Font::with_name("Consolas"),
         ..iced::settings::Settings::default()
     };
     iced::application(TitleView, State::update, State::view)
         .settings(s)
+        .subscription(State::subscription)
         .run()
 }
 
@@ -21,7 +26,10 @@ struct State {
     value: i64,
     slider: i32,
     content: String,
+    server_text: String,
     si: systeminfo::Scene,
+    handle: Option<server::ThreadHandles>,
+    ch: Option<server::UiCh>,
 }
 
 impl Default for State {
@@ -30,8 +38,11 @@ impl Default for State {
             value: 0,
             slider: 0,
             content: "test-app".to_string(),
+            server_text: "".to_string(),
             screen: Screen::Welcome,
             si: systeminfo::Scene::new(),
+            handle: None,
+            ch: None,
         }
     }
 }
@@ -53,16 +64,33 @@ impl iced::application::Title<State> for TitleView {
 #[derive(Debug, Clone)]
 enum Message {
     SystemInfo(SystemInfoMsg),
+    ServerStart,
+    ServerStop,
     Increment,
     Decrement,
     SliderChanged(i32),
     ContentChanged(String),
+    #[allow(dead_code)]
+    Tick(Instant),
 }
 
 impl State {
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::SystemInfo(s) => return self.si.update(s).map(Message::SystemInfo),
+            Message::ServerStart => {
+                if self.handle.is_none() {
+                    let (h, ch) = server::spawn();
+                    self.handle = Some(h);
+                    self.ch = Some(ch);
+                }
+            }
+            Message::ServerStop => {
+                if let Some(h) = self.handle.take() {
+                    h.process.cancel();
+                    h.handle.join().unwrap().unwrap();
+                }
+            }
             Message::Increment => {
                 self.value += 1;
             }
@@ -74,6 +102,17 @@ impl State {
             }
             Message::ContentChanged(content) => {
                 self.content = content;
+            }
+            Message::Tick(_) => {
+                if let Some(ch) = &mut self.ch {
+                    while let Ok(msg) = ch.rx.try_recv() {
+                        match msg {
+                            server::Response::Tick(elapsed) => {
+                                self.server_text = format!("Elapsed: {:?}", elapsed);
+                            }
+                        }
+                    }
+                }
             }
         };
         Task::none()
@@ -109,7 +148,10 @@ impl State {
                 .padding(10)
                 .size(14),
             slider(-100..=100, self.slider, Message::SliderChanged),
-            text(self.slider).size(14)
+            text(self.slider).size(14),
+            button("Start server").on_press(Message::ServerStart),
+            button("Stop server").on_press(Message::ServerStop),
+            text(&self.server_text).size(14),
         ]
         .spacing(20)
         .width(200);
@@ -134,4 +176,19 @@ impl State {
             }
         }
     }
+
+    fn subscription(&self) -> Subscription<Message> {
+        window::frames().map(Message::Tick)
+    }
+}
+
+fn init() {
+    use tracing_subscriber::layer::SubscriberExt;
+    use tracing_subscriber::util::SubscriberInitExt;
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 }
